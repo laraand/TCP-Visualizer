2aba61663e468bc9f5e74048d7b8e4dae29a5bd6
Andrea <anlara@csumb.edu>
2019-03-02 11:41:48 -0800

    changed window and title

--- tcp_visualizer/tcp_visualizer.py
+++ tcp_visualizer/tcp_visualizer.py
@@ -1,27 +1,24 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QIcon
-
-
-class Example(QWidget):
-    
+ 
+class App(QWidget):
+ 
     def __init__(self):
         super().__init__()
-        
+        self.title = 'TCP Visualizer'
+        self.left = 0
+        self.top = 0
+        self.width = 1240
+        self.height = 780
         self.initUI()
-        
-        
+ 
     def initUI(self):
-        
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('Icon')
-        self.setWindowIcon(QIcon('web.png'))        
-    
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
         self.show()
-        
-        
+ 
 if __name__ == '__main__':
-    
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = App()
     sys.exit(app.exec_())
