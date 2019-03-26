cfecc866868944d12b23775bca019b48a278d92f
Andrea <anlara@csumb.edu>
2019-03-26 12:59:42 -0700

    made changes to file, in order for it to run properly using QtGui

--- tcp_visulizer.py
+++ tcp_visulizer.py
@@ -1,13 +1,14 @@
 import sys
 <<<<<<< HEAD
 from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout,QLabel
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout,QLabel, QSizePolicy, QMessageBox, QWidget
+from PyQt5.QtGui import QIcon
 =======
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout,QLabel, QSizePolicy, QMessageBox, QWidget
 from PyQt5.QtGui import QIcon
->>>>>>> mayraochoa13
+
+>>>>>>> b197828b53774a5940dd8c40cb50d52248cfdd5f
 from PyQt5.QtCore import pyqtSlot
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
