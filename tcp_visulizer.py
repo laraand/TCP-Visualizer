e826e7f9362c5f01806867ef04e845e6b3692afc
Andrea <anlara@csumb.edu>
2019-04-24 19:07:15 -0700

    images used for handshake

--- tcp_visulizer.py
+++ tcp_visulizer.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout,QLabel, QSizePolicy, QMessageBox, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QFormLayout, QScrollArea
+from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import pyqtSlot
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -12,85 +13,125 @@ import random
 import array
 
 
+
 class App(QDialog):
  
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("TCP Visualizer")
-        self.screen = QtWidgets.QDesktopWidget().setGeometry(50,50,600,400)
+        self.title = "TCP Visualizer"
+        self.screen = QtWidgets.QDesktopWidget().setGeometry(800,700,800,700)
         self.screen = QtWidgets.QDesktopWidget().screenGeometry(-1)
 
-        self.handshake()
-        self.initUI()
         
-    def initUI(self):
+        #self.initUI()
+        
+    #def initUI(self):
+        #logo icon for window
+        self.setWindowIcon(QtGui.QIcon("C:\\Users\\Andrea\\Documents\\GitHub\\TCP-Visualizer\\TCPupdatedlogo.png"))
+        self.setWindowTitle(self.title)
         
        
         RTTlabel = QLabel('RTT',self)
-        RTTlabel.move(200,50)
+        RTTlabel.move(1000,50)
         RTTlabel.setStyleSheet("font: 20pt Comic Sans MS")
 
-        HndShakelabel = QLabel('Handshake',self)
-        HndShakelabel.move(1300,50)
-        HndShakelabel.setStyleSheet("font: 20pt Comic Sans MS")
+        HandShakeLabel = QLabel('Handshake', self)
+        HandShakeLabel.setStyleSheet("font: 20pt Comic Sans MS")
+        HandShakeLabel.move(200,55)
 
-        buttonPlay = QPushButton('',self)
-        buttonPlay.setIcon(QtGui.QIcon('C:\\Users\\Andrea\\Documents\\GitHub\\TCP-Visualizer\\play.jpg'))
-        buttonPlay.setIconSize(QtCore.QSize(62,62))
-        buttonPlay.move(1286, 898)
+        
 
-        buttonRewind = QPushButton('',self)
-        buttonRewind.setIcon(QtGui.QIcon('C:\\Users\\Andrea\\Documents\\GitHub\\TCP-Visualizer\\backward.png'))
-        buttonRewind.setIconSize(QtCore.QSize(62,62))
-        buttonRewind.move(1212, 898)
+        inc = 0
+        inc2 = 0
+        
+        self.image2 = QLabel(self)
+        self.image3 = QLabel(self)
+        
+        groupBox = QGroupBox()
+        formLayout = QFormLayout()
+        
+        #create widget for handshake vertical lines
+        for x in range(10):
+            inc = inc + 100
+            inc2 = inc2 + 200
+            
+            #self.image2 = QLabel(self)
+            self.image2.setPixmap(QPixmap('C:\\Users\\Andrea\\Documents\\GitHub\\TCP-Visualizer\\right.png'))
+            self.image2.setGeometry(1350,0+inc,700,400)
 
-        buttonForward = QPushButton('',self)
-        buttonForward.setIcon(QtGui.QIcon('C:\\Users\\Andrea\\Documents\\GitHub\\TCP-Visualizer\\forward.png'))
-        buttonForward.setIconSize(QtCore.QSize(62,62))
-        buttonForward.move(1354, 898)
+            #self.image3 = QLabel(self)
+            self.image3.setPixmap(QPixmap('C:\\Users\\Andrea\\Documents\\GitHub\\TCP-Visualizer\\left.png'))
+            self.image3.setGeometry(1350,0+inc2,600,400)
 
+            formLayout.addRow(self.image2)
+            formLayout.addRow(self.image3)
 
-               
+            formLayout.addRow(self.image2)
+            formLayout.addRow(self.image3)
+
+        groupBox.setLayout(formLayout)
+        scroll = QScrollArea()
+        scroll.setWidget(groupBox)
+        scroll.setWidgetResizable(True)
+        scroll.setFixedHeight(700)
+        scroll.setFixedWidth(510)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(scroll)
+        
+        #call handshake definition, in order to obtain data
+        #self.handshake()
+        
         self.show()
 
     def handshake(self):
-        print('handshake def working to this point')
-        
         #open file in which parsed information is stored in 
         file = open("pckts.txt", "r")
-        print('file was opened')
+        
         #read the contents of the file
         contents = file.readlines()
-        print('file was read')
+        
 
         #variables to hold searching content within parsed file
-        a = 'Acknowledgment number: '
-        s = 'Next sequence number: '
-        #array's to store acknowledgment and sequence numbers
+        ack = 'Acknowledgment number: '
+        seq = 'Next sequence number: '
+        window = 'Window size value: '
+        
+        #array's to store acknowledgment,sequence numbers and window size
         seqPk = []
         ackPk = []
+        wndwSize = [] 
         
         with open("C:\\Users\\Andrea\\Desktop\\pckts.txt", "r") as pack:
             
             for line in pack:
                 #condition to search for acknowledgment number
-                if(a in line):
+                if(ack in line):
                     #obtain acknowledgment number
                     ackLength = line[23:(len(line)-26)]
                     #print ('acknowledge: ' + ackLength)
                     ackPk.append(ackLength)
 
                 #condition to search for sequence number
-                if(s in line):
+                if(seq in line):
                     #obtain sequence number
                     seqLength = line[22:(len(line)-30)]
-                    #print ('seq: ' + seqLength)
+                    #append value to sequence array
                     seqPk.append(seqLength)
+
+                #condition to obtain window size
+                if(window in line):
+                    #obtain window size value
+                    windowLength = line[19:]
+                    #append value to sequence array
+                    wndwSize.append(windowLength)
+		
+                #close file
                 file.close()
-                
-            print(seqPk[0])
+
+            
             for i in range(len(ackPk)):
-                print('Downarrow' + ackPk[i] + seqPk[i])
+                print( ackPk[i] + seqPk[i] + wndwSize[i])
             
         self.show()
  
